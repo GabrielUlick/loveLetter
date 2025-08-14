dc59dc4764214477723c1444c7702436f3991099
GabrielUlick <gabrieludb@hotmail.com>
2025-08-14 00:25:23 -0300

    feat: enhance galaxy background with nebula layer and additional star layers

--- src/components/shared/GalaxyBackground.tsx
+++ src/components/shared/GalaxyBackground.tsx
@@ -1,8 +1,24 @@
 const GalaxyBackground = () => (
-	<div className="galaxy-background">
-		<div className="stars-1"></div>
-		<div className="stars-2"></div>
-		<div className="stars-3"></div>
+		<div className="galaxy-background" aria-hidden>
+		{/* Soft drifting nebula clouds */}
+		<div className="nebula-layer" />
+
+		{/* Twinkling parallax starfields */}
+			<div className="stars-1" />
+			<div className="stars-2" />
+			<div className="stars-3" />
+			<div className="stars-4" />
+			<div className="stars-5" />
+
+		{/* Occasional shooting stars */}
+		<div className="meteors">
+			<span />
+			<span />
+			<span />
+			<span />
+			<span />
+			<span />
+		</div>
 	</div>
 );
 
